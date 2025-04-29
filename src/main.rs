58ceb25c69212d8545aa3eca661d2602f5542604
ArunJot <arunjot@antiersolutions.com>
2025-04-29 17:48:04 +0530

    added hello world

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
 #[macro_use] extern crate rocket;
-use rocket::serde::{Serialize, json::Json};
 
-#[derive(Serialize)]
-struct Message {
-    content: String,
-}
-
-#[get("/message")]
-fn message() -> Json<Message> {
-    Json(Message {
-        content: "Hello from Rust!".to_string(),
-    })
+#[get("/")]
+fn index() -> &'static str {
+    "Hello in rust, world!"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![message])
+    rocket::build().mount("/", routes![index])
 }
