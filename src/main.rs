e1da2a0cdab8098e5ee94003ca32dad5364de370
ArunJot <arunjot@antiersolutions.com>
2025-05-02 11:53:05 +0530

    Added the post method

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use] extern crate rocket;
 use rocket::fs::{FileServer, relative};
 use rocket::serde::json::Json;
-use rocket::serde::{Serialize};
+use rocket::serde::{Serialize,Deserialize};
 
 // Define a simple struct to send as JSON
 #[derive(Serialize)]
@@ -10,6 +10,15 @@ struct Info {
     title: String,
     message: String,
 }
+
+// New struct to receive POSTed JSON
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct InputData {
+    name: String,
+    feedback: String,
+}
+
 
 // Return a JSON object at GET /data
 #[get("/data")]
@@ -20,10 +29,20 @@ fn data() -> Json<Info> {
     })
 }
 
+// POST endpoint
+#[post("/submit", data = "<input>")]
+fn submit(input: Json<InputData>) -> Json<Info> {
+    Json(Info {
+        title: format!("Hello, {}!", input.name),
+        message: format!("Your feedback was: '{}'", input.feedback),
+    })
+}
+
+
 // Launch the Rocket app and serve static + routes
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", FileServer::from(relative!("static"))) // Serves index.html
-        .mount("/api", routes![data])                      // API route
+        .mount("/api", routes![data, submit])                    // API route
 }
